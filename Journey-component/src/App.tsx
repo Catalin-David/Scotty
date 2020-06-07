3b1403ffaa2d0f2497c2c543fa58beca7db2a90a
Catalin-David <cata02dav@yahoo.com>
2020-06-07 19:40:06 +0300

    Changed the JourneyCard order of items to image/title/properties and removed the hover effect

--- Journey-component/src/App.tsx
+++ Journey-component/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
 import Journeys from './components/JourneysOverview/JourneysOverview';
-import { initializeIcons } from '@uifabric/icons';
 import styles from './App.module.css';
-import { Icon } from '@fluentui/react/lib/Icon';
 
 function App() {
-  initializeIcons()
   return (
     <div className={styles.App}>
       <Header />
