8fdec0e650ba7a5693200014d04398fa7bffede3
Catalin-David <cata02dav@yahoo.com>
2020-06-07 17:30:44 +0300

    Put the function that apply the provisioning template into ListItemsProvisioning.ps1, added ListItems.xml file in which the extracted template is stored and imported the ListItemsProvisioning.ps1 functions into Main.ps1

---
+++ Provisioning/ListItemsProvisioning.ps1
@@ -0,0 +1,16 @@
+function ConnectToReceivingTenant(){
+    # connect to receiving tenant
+
+    [xml]$ConfigFile = Get-Content "Config.xml"
+
+    $siteURL = $ConfigFile.Credentials.ReceivingWebsite
+    $username = $ConfigFile.Credentials.Username
+
+    $Credential = Get-Credential -UserName $username -Message "Type password here"
+    Connect-PnpOnline -Url $siteURL -Credentials $Credential
+}
+
+function AddListItems(){
+    # add items to the receiving tenant
+    Apply-PnpProvisioningTemplate ListItems.xml
+}
--- Provisioning/Main.ps1
+++ Provisioning/Main.ps1
@@ -1,82 +1,47 @@
 #import other files representing granular actions
-function AddListItems(){
-    Apply-PnpProvisioningTemplate ListItems.xml
-}
-#get data from config.xml
-#connect to tenant
 
-function Connect-SPOSiteApproach() {
-    $userName = "DavidCatalin@davidcatalin.onmicrosoft.com"
-    $site = "https://davidcatalin-admin.sharepoint.com"
-    $password = "-password-here-"
-    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+function ConnectToProvisioningTenant(){
+    #connect to provisioning tenant
 
-    if ((Get-Module Microsoft.Online.SharePoint.PowerShell).Count -eq 0) {
-        Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
-    }
-
-    $cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $userName, $securePassword
-    Connect-SPOSite -Url $site -Credential $cred
-}
-
-function ConnectContextApproach(){
-    $siteURL = "https://davidcatalin.sharepoint.com/sites/AccesaNews"
-    $username = "DavidCatalin@davidcatalin.onmicrosoft.com"
-    $password = "-password-here-"
-    
-    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-
-    $Credential = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword)
-    $Context = New-Object Microsoft.SharePoint.Client.ClientContext($siteURL)
-    $Context.Credentials = $Credential
-
-    $SPOWeb = $Context.Web
-    $Context.load($SPOWeb)
-    Get-PnpListItem -List MyNewList
-}
-
-function Connect-PnpOnlineApproach(){
     [xml]$ConfigFile = Get-Content "Config.xml"
 
 
     $siteURL = $ConfigFile.Credentials.Website
     $username = $ConfigFile.Credentials.Username
-    #$password = "-password-here-"
-    #$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 
     $Credential = Get-Credential -UserName $username -Message "Type password here"
     Connect-PnPOnline -Url $siteURL -Credentials $Credential
 }
 
 function GetProvisioningTemplateToXml(){
-    Get-PnPProvisioningTemplate -Out ListItems.xml -ListsToExtract "MyNewList"
-
-    Add-PnPDataRowsToProvisioningTemplate -Path ListItems.xml -List "MyNewList" -Query "<view></view>"
-}
-
-function ConnectToNewTenant(){
+    # receive template of provisioning tenant
     [xml]$ConfigFile = Get-Content "Config.xml"
 
-    $siteURL = $ConfigFile.Credentials.ReceivingWebsite
-    $username = $ConfigFile.Credentials.Username
+    $listToBeExtracted = $ConfigFile.Credentials.ListToBeExtracted
 
-    $Credential = Get-Credential -UserName $username -Message "Type password here"
-    Connect-PnpOnline -Url $siteURL -Credentials $Credential
+    Get-PnPProvisioningTemplate -Out ListItems.xml -Handlers Lists -ListsToExtract $listToBeExtracted
+}
+
+function AddItemsToProvisioningTemplate(){
+    [xml]$ConfigFile = Get-Content "Config.xml"
+
+    $listToBeAddedItems = $ConfigFile.Credentials.ListToBeExtracted
+
+    Add-PnPDataRowsToProvisioningTemplate -Path ListItems.xml -List $listToBeAddedItems -Query "<view></view>"
 }
 
 function Main(){
-    #AddListItems
-    #Connect-SPOSiteApproach
-    #ConnectContextApproach
-    Connect-PnpOnlineApproach
-
+    ConnectToProvisioningTenant
 
     GetProvisioningTemplateToXml
 
-    ConnectToNewTenant
+    AddItemsToProvisioningTemplate
+
+    . .\ListItemsProvisioning.ps1
+
+    ConnectToReceivingTenant
 
     AddListItems
 }
-#call functions from the other files
 
 Main
