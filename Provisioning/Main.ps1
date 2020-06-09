c7e49fbd3ed36215e44c82b34873272d24f222ec
Catalin-David <cata02dav@yahoo.com>
2020-06-09 16:24:30 +0300

    Fuctions for connecting to the tenant added

--- Provisioning/Main.ps1
+++ Provisioning/Main.ps1
@@ -1,6 +1,29 @@
 #import other files representing granular actions
+. .\2_TermGroupProvisioning.ps1
+function ConnectToProvisioningTenant(){
+    #connect to provisioning tenant
 
-#get data from config.xml
-#connect to tenant
+    [xml]$ConfigFile = Get-Content "Config.xml"
 
-#call functions from the other files
+
+    $siteURL = $ConfigFile.Credentials.Website
+    $username = $ConfigFile.Credentials.Username
+
+    $Credential = Get-Credential -UserName $username -Message "Type password here"
+    Connect-PnPOnline -Url $siteURL -Credentials $Credential
+}
+
+function GetProvisioningTemplateToXml(){
+    # receive template of provisioning tenant
+    [xml]$ConfigFile = Get-Content "Config.xml"
+
+    $listToBeExtracted = $ConfigFile.Credentials.ListToBeExtracted
+
+    Get-PnPProvisioningTemplate -Out ListItems.xml -Handlers Lists -ListsToExtract $listToBeExtracted
+}
+
+function Main(){
+
+}
+
+Main
