694ea49cc1c497d71b556c368036ac6da778b72a
Catalin-David <cata02dav@yahoo.com>
2020-06-16 00:24:20 +0300

    Switch variable names to camelCase, solved issue with repeating code and wrote just one function for connecting to a sharepoint website. Declared configFile as a script-scoped variable in order not to repeat code.

--- Provisioning/1_ListItemsProvisioning.ps1
+++ Provisioning/1_ListItemsProvisioning.ps1
@@ -1,17 +1,6 @@
 #list items provisioning function
-function ConnectToReceivingTenant(){
-    # connect to receiving tenant
 
-    [xml]$ConfigFile = Get-Content "Config.xml"
-
-    $siteURL = $ConfigFile.Credentials.ReceivingWebsite
-    $username = $ConfigFile.Credentials.Username
-
-    $Credential = Get-Credential -UserName $username -Message "Type password here"
-    Connect-PnpOnline -Url $siteURL -Credentials $Credential
-}
-
-function AddListItems(){
-    # add items to the receiving tenant
+function AddListFromXmlToSharepointSite(){
+    # adds the list found inside the provisiong template stored in ListItems.xml to the sharepoint site to which we are currently connected
     Apply-PnpProvisioningTemplate ListItems.xml
 }
--- Provisioning/Main.ps1
+++ Provisioning/Main.ps1
@@ -1,46 +1,47 @@
-#import other files representing granular actions
 . .\1_ListItemsProvisioning.ps1
-function ConnectToProvisioningTenant(){
-    #connect to provisioning tenant
 
-    [xml]$ConfigFile = Get-Content "Config.xml"
-
-
-    $siteURL = $ConfigFile.Credentials.Website
-    $username = $ConfigFile.Credentials.Username
-
-    $Credential = Get-Credential -UserName $username -Message "Type password here"
-    Connect-PnPOnline -Url $siteURL -Credentials $Credential
-}
-
-function GetProvisioningTemplateToXml(){
-    # receive template of provisioning tenant
-    [xml]$ConfigFile = Get-Content "Config.xml"
-
-    $listToBeExtracted = $ConfigFile.Credentials.ListToBeExtracted
-
-    Get-PnPProvisioningTemplate -Out ListItems.xml -Handlers Lists -ListsToExtract $listToBeExtracted
-}
-
-function AddItemsToProvisioningTemplate(){
-    [xml]$ConfigFile = Get-Content "Config.xml"
-
-    $listToBeAddedItems = $ConfigFile.Credentials.ListToBeExtracted
-
-    Add-PnPDataRowsToProvisioningTemplate -Path ListItems.xml -List $listToBeAddedItems -Query "<view></view>"
-}
-
-function Main(){
-    ConnectToProvisioningTenant
-
-    GetProvisioningTemplateToXml
-
-    AddItemsToProvisioningTemplate
-
-
-    ConnectToReceivingTenant
-
-    AddListItems
-}
+[xml]$script:configFile = Get-Content "Config.xml"
 
 Main
+
+function Main(){
+
+    ConnectToSharepointSite($script:configFile.Credentials.ProvisioningWebsiteUrl)
+
+    GetProvisioningTemplateOfListAsXml
+
+    AddItemsToProvisioningTemplateOfListFromXml
+
+    ConnectToSharepointSite($script:configFile.Credentials.ReceivingWebsiteUrl)
+
+    AddListFromXmlToSharepointSite
+}
+
+function ConnectToSharepointSite($url){
+    #connect to a sharepoint site with the url given as a parameter
+
+    $username = $script:configFile.Credentials.Username
+    $passwordPath = $script:configFile.Credentials.PasswordPath
+
+    $password = Get-Content $passwordPath | ConvertTo-SecureString
+
+    $credentials = New-Object System.Management.Automation.PSCredential($username, $password)
+
+    Connect-PnPOnline -Url $url -Credentials $credentials
+}
+
+function GetProvisioningTemplateOfListAsXml(){
+    # stores the provisioning template of the list whose name is found at ListToBeExtraced field from Config.xml into ListItems.xml
+
+    $listToBeExtracted = $script:configFile.Credentials.ListToBeExtracted
+
+    Get-PnPProvisioningTemplate -Out ListItems.xml -Handlers Lists -ListsToExtract $listToBeExtracted -Force
+}
+
+function AddItemsToProvisioningTemplateOfListFromXml(){
+    # adds items to the List from the provisioning template which can be found in ListItems.xml
+
+    $listToWhichItemsAreAdded = $script:configFile.Credentials.ListToBeExtracted
+
+    Add-PnPDataRowsToProvisioningTemplate -Path ListItems.xml -List $listToWhichItemsAreAdded -Query "<view></view>"
+}
