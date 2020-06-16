793f2550070f6db3f64b51e1ad4b6b8fae396804
Catalin-David <cata02dav@yahoo.com>
2020-06-16 11:35:36 +0300

    Combined the ConnectToSite and ConnectToTenant functions into a single function called ConnectToSharepointUrl as they had exactly the same code, created AddDocumentLibraryToSharepointSite and deleted comments and other lines of code whose purpose was testing the code

--- Provisioning/2_TermGroupProvisioning.ps1
+++ Provisioning/2_TermGroupProvisioning.ps1
@@ -1,11 +1,9 @@
-#term group provisioning function
-
 function ImportTermGroupFromXmlToSharepointTenant(){
     # takes the termgroup stored in TermGroup.xml and adds it to the currently connected sharepoint tenant
     Apply-PnPProvisioningTemplate -Path TermGroup.xml
 }
 
-#create document library function
-function AddDocumentLibrary(){
-    New-PnPList -Title "Skill resources" -Template DocumentLibrary -OnQuickLaunch
+function AddDocumentLibraryToSharepointSite(){
+    # takes the document library stored in DocumentLibraryTemplate.xml and adds it to the currently connected sharepoint site
+    Apply-PnPProvisioningTemplate -Path .\DocumentLibraryTemplate.xml
 }
--- Provisioning/Main.ps1
+++ Provisioning/Main.ps1
@@ -1,4 +1,3 @@
-#import other files representing granular actions
 . .\2_TermGroupProvisioning.ps1
 
 [xml]$script:configFile = Get-Content "Config.xml"
@@ -6,22 +5,25 @@
 Main
 
 function Main(){
-    #ConnectToSharepointTenant($script:configFile.Credentials.TenantUrl)
 
-    #Get-PnPProvisioningTemplate -Out .\ListItems.xml -Force
-    #ImportTermGroupFromXmlToSharepointTenant
-    #AddDocumentLibrary
+    ConnectToSharepointUrl($script:configFile.Credentials.TenantUrl)
+
+    ImportTermGroupFromXmlToSharepointTenant
+
+    ConnectToSharepointUrl($script:configFile.Credentials.SiteUrl)
+
+    AddDocumentLibraryToSharepointSite
 }
 
-function ConnectToSharepointTenant($url){
-    #connect to sharepoint tenant with the url given as a parameter
+function ConnectToSharepointUrl($url){
+    #connect to sharepoint tenant or site with the url given as a parameter
 
     $username = $script:configFile.Credentials.Username
-    $passwordPath = $script:configFile.Credentials.PasswordPath 
+    $passwordPath = $script:configFile.Credentials.PasswordPath
+
     $password = Get-Content $passwordPath | ConvertTo-SecureString
 
     $credentials = New-Object System.Management.Automation.PSCredential($username, $password)
 
-    #$Credential = Get-Credential -UserName $username -Message "Type password here"
     Connect-PnPOnline -Url $url -Credentials $credentials
 }
